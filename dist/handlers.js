a7d74b184509a469a3fac65385b1e7948f0e0767
Alex Guo <alexguo@audible.com>
2015-09-17 15:32:30 -0400

    didn't run babel

--- dist/handlers.js
+++ dist/handlers.js
@@ -7,15 +7,15 @@ Object.defineProperty(exports, "__esModule", {
   value: true
 });
 var Handlers = {
-  "10": function _() {
+  "10.00": function _() {
     console.log("Ten!");
   },
 
-  "20": function _() {
+  "20.00": function _() {
     console.log("Twenty!");
   },
 
-  "30": function _() {
+  "30.00": function _() {
     console.log("Thirty!");
   }
 };
